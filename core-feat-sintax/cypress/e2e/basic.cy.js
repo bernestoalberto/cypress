0aa0adbb3bb861c77cd096913b7b7103b1fd1940
Ernesto <bernestoalberto@gmail.com>
2025-06-23 19:09:07 -0400

    feat: Initialize React application with routing and contact form
    - Added main application structure with routing using React Router.
    - Created Home and About pages.
    - Implemented a Contact Form component with validation and submission handling.
    - Styled components using CSS modules.
    - Added a Header component for navigation.
    - Set up global styles and CSS variables for consistent theming.
    - Configured Vite as the build tool for the project.
    - Created Cypress configuration for end-to-end testing.
    - Added initial Cypress tests and fixtures for testing the application.

---
+++ core-feat-sintax/cypress/e2e/basic.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("task page", () => {
+	it("should render the main image", () => {
+		cy.visit("http://localhost:5173/");
+		cy.get(".main-header img");
+		cy.get(".main-header").find("img");
+	});
+
+	it("should display the page title", () => {
+		cy.visit("http://localhost:5173/");
+    cy.get("h1").should("have.length", 1);
+		cy.get("h1").contains("My Cypress Course Tasks");
+	});
+});
